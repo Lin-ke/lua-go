2f15e87f31fa6c154683d81921b9d45d2c1e8daa
Lin-Ke <1720587660@qq.com>
2022-12-05 23:21:43 +0800

    add stack api

---
+++ state/lua_stack.go
@@ -0,0 +1,86 @@
+package state
+
+type luaStack struct {
+	slots []luaValue
+	top   int
+}
+
+func newLuaStack(size int) *luaStack {
+	return &luaStack{
+		slots: make([]luaValue, size),
+		top:   0,
+	}
+}
+
+/*
+检查枝 的 空 闲 空间是否还可以容纳（推入）至少 n 个值，如果不满足这个条 件 ， 则 调用
+Go 语 言 内 置的 append （） 函数进行扩容
+*/
+func (self *luaStack) check(n int) {
+	free := len(self.slots) - self.top
+	for i := free; i < n; i++ {
+		self.slots = append(self.slots, nil)
+	}
+}
+
+// 需要考虑线程么？
+func (self *luaStack) push(val luaValue) {
+	if self.top == len(self.slots) {
+		panic("stack overflow!")
+	}
+	self.slots[self.top] = val
+	self.top++
+}
+
+func (self *luaStack) pop() luaValue {
+	if self.top < 1 {
+		panic("stack underflow!")
+	}
+	self.top--
+	val := self.slots[self.top]
+	self.slots[self.top] = nil
+	return val
+}
+
+// idx -1 ==> self.top
+// 到后面会完善
+
+func (self *luaStack) absIndex(idx int) int {
+	if idx >= 0 {
+		return idx
+	}
+	return idx + self.top + 1
+}
+
+func (self *luaStack) isValid(idx int) bool {
+	absIdx := self.absIndex(idx)
+	return absIdx > 0 && absIdx <= self.top
+}
+
+func (self *luaStack) get(idx int) luaValue {
+	absIdx := self.absIndex(idx)
+	if absIdx > 0 && absIdx <= self.top {
+		return self.slots[absIdx-1]
+	}
+	return nil
+}
+
+func (self *luaStack) set(idx int, val luaValue) {
+	absIdx := self.absIndex(idx)
+	if absIdx > 0 && absIdx <= self.top {
+		self.slots[absIdx-1] = val // 设定开始是1的问题
+		return
+	}
+	panic("invalid index!")
+}
+
+// 反转
+// 注意这些index是go的（0-> top-1)
+func (self *luaStack) reverse(from, to int) {
+	slots := self.slots
+	for from < to {
+		slots[from], slots[to] = slots[to], slots[from]
+		from++
+		to--
+	}
+}
