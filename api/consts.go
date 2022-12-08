ede2e583979329791dc11bf1b607369582476830
Lin-Ke <1720587660@qq.com>
2022-12-08 15:33:05 +0800

    arithop

--- api/consts.go
+++ api/consts.go
@@ -13,3 +13,21 @@ const (
 	LUA_TUSERDATA
 	LUA_TTHREAD
 )
+
+/* arithmetic functions */
+const (
+	LUA_OPADD  = iota // +
+	LUA_OPSUB         // -
+	LUA_OPMUL         // *
+	LUA_OPMOD         // %
+	LUA_OPPOW         // ^
+	LUA_OPDIV         // /
+	LUA_OPIDIV        // //
+	LUA_OPBAND        // &
+	LUA_OPBOR         // |
+	LUA_OPBXOR        // ~
+	LUA_OPSHL         // <<
+	LUA_OPSHR         // >>
+	LUA_OPUNM         // -
+	LUA_OPBNOT        // ~
+)
--- api/lua_state.go
+++ api/lua_state.go
@@ -1,7 +1,8 @@
 package api
 
 type LuaType = int
-
+type ArithOp = int
+type CompareOp int
 type LuaState interface {
 	/* basic stack manipulation */
 	GetTop() int
